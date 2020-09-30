c7402bed158345d0c73dcad0a08af157293371dd
victoreh <victor.eh@nuvolar.eu>
2020-09-30 11:43:55 +0200

    finished without inputs

--- src/app/vehicles-board/timeline/timeline.component.ts
+++ src/app/vehicles-board/timeline/timeline.component.ts
@@ -1,49 +1,42 @@
 import { Mechanic } from './../models/mechanic';
 import { Timeline, DataGroup, DataGroupCollectionType } from 'vis-timeline';
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { Vehicle } from '../models/vehicle';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, combineLatest } from 'rxjs';
+import { AppState } from '../models/app-state';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  styleUrls: ['./timeline.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class TimelineComponent implements OnChanges {
-  @Input() vehicleItems: Observable<Array<Vehicle>>;
-  @Input() mechanicItems: Observable<Array<Mechanic>>;
+export class TimelineComponent {
 
-  vehicles: Vehicle[];
-  mechanics: Mechanic[];
+  constructor(private store: Store<AppState>) {
 
-  constructor() {
-  }
+    const vehicles = this.store.select(store => store.vehicle.list);
+    const mechanics = this.store.select(store => store.mechanic.list);
 
-  ngOnChanges(changes: SimpleChanges): void {
-
-    // doing one at a time because OBSERVABLES can resolve one at a time
-
-    if (changes.vehicleItems && changes.vehicleItems.currentValue.length > 0) {
-      this.vehicles = changes.vehicleItems.currentValue;
-    }
-    if (changes.mechanicItems && changes.mechanicItems.currentValue.length > 0) {
-      this.mechanics = changes.mechanicItems.currentValue;
-    }
-    if (this.vehicles && this.mechanics) {
-      this.generateTL(this.vehicles, this.mechanics);
-    }
+    const data = combineLatest([
+      vehicles,
+      mechanics
+    ])
+      .pipe(
+        map(([vehArray, mechArray]) => {
+          // combineLatest returns an array of values, here we map those values to an object
+          return { vehArray, mechArray };
+        })
+      );
+    data.subscribe(r => {
+      this.generateTL(r.vehArray, r.mechArray);
+    });
   }
 
   generateTL(vehicles: Vehicle[], mechanics: Mechanic[]): void {
     const container = document.getElementById('visualization');
-
-    // TIMELINE not getting items if in DATASET FORMAT (pending)
-    // let items = new DataSet([]);
-    // vehicles.forEach(vehicle => {
-    //   items.add(
-    //     vehicle
-    //   );
-    // });
 
     const options = {
       groupOrder: 'content',
@@ -51,9 +44,7 @@ export class TimelineComponent implements OnChanges {
     };
 
     if (container && vehicles.length !== 0 && mechanics.length !== 0) {
-
       const timeline = new Timeline(container, vehicles, mechanics as DataGroupCollectionType, options);
-
     }
   }
 
